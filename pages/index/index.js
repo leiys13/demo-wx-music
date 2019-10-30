3c9aa441aa216e3c8445a492f179abfe6ff2163c
leiys13 <leiys13@icloud.com>
2019-10-31 00:25:01 +0800

    播放音乐示例

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,19 @@ Page({
     tab: 0
   },
 
+  onReady: function() {
+    var audio = wx.createInnerAudioContext()
+    audio.src = '/music/酸酸甜甜就是我.mp3'
+    audio.onPlay(function() {
+      console.log('开始播放 酸酸甜甜就是我-张含韵 ... http')
+    })
+    audio.onError(function(res) {
+      console.log('errMsg: ', res.errMsg)
+      console.log('errCode: ', res.errCode)
+    })
+    audio.play()
+  },
+
   /**
    * 事件类函数
    */
